12e09c4b0eecab290e5dc21127bd01ba8879678f
theCoder-lab <jandarwish13@gmail.com>
2023-10-15 04:35:17 +0200

    connection to database hass been added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function Projects() {
+  const [dataList, setDataList] = useState([]);
+  const collectionRef = collection(db, "posts");
+
+  const getProjects = async () => {
+    const data = await getDocs(collectionRef);
+    setDataList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
 
   return (
     <>
-        <section className="projects" id="projects">
+      <section className="projects" id="projects">
         <h2 className="section-title">Projects</h2>
         <div className="container" id="section-offset">
+          {dataList.map((item) => {
+            return (
+              <div key={item.id}>
+                <div
+                  className="box"
+                  data-aos="zoom-in-down"
+                  data-aos-delay={item.aos}
+                >
+                  <img loading="lazy" src="img/coding-2.jpg" alt="temp" />
 
-            <div className="box" data-aos="zoom-in-down">
+                  <h3>{item.title}</h3>
+                  <p>{item.post}</p>
+                  <button type="submit">Preview</button>
+                </div>
+              </div>
+            );
+          })}
+
+          {/*             <div className="box" data-aos="zoom-in-down">
                 
                     <img loading="lazy"  src="img/coding-2.jpg" alt="temp" />
                 
@@ -79,12 +109,11 @@ function Projects() {
                     consequatur esse neque voluptatem!
                 </p>
                 <button type="submit">Preview</button>
-            </div>
-
+            </div> */}
         </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
