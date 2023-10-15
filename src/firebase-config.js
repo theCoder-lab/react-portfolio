323c43ba0d57c162ae3a92d4db5e51b490d82897
theCoder-lab <jandarwish13@gmail.com>
2023-10-15 04:56:12 +0200

    connection to database hass been added

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBoYOyO1p4iAI9K6uChtXxBthQ8b0qoPxA",
+  authDomain: "blog-test-d1960.firebaseapp.com",
+  projectId: "blog-test-d1960",
+  storageBucket: "blog-test-d1960.appspot.com",
+  messagingSenderId: "86602397577",
+  appId: "1:86602397577:web:cb52f2d59cdce7438a1358"
 };
 
 // Initialize Firebase
