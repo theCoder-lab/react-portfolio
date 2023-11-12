e9896b113ee01f637a3bc56570cd4ee6d0adc1fa
theCoder-lab <jandarwish13@gmail.com>
2023-11-12 19:23:22 +0100

    new feature has been added load more projects functionality

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,18 +10,27 @@ function Projects() {
     const data = await getDocs(collectionRef);
     setDataList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
-
+ 
   useEffect(() => {
     getProjects();
   }, []);
 
+
+  const [itemsID, setItemsID] = useState(3);
+
+  const handleLoadMore = () => {
+    setItemsID(prevItemsID => prevItemsID + 4);
+  };
+
   return (
+    
     <>
       <section className="projects" id="projects">
         <h2 className="section-title">Projects</h2>
         <div className="container" id="section-offset">
-          {dataList.map((item) => {
+          {dataList.slice(0, itemsID).map((item) => {
             return (
+              <>
               <div key={item.id}>
                 <div
                   className="box"
@@ -35,81 +44,19 @@ function Projects() {
                   <button type="submit">Preview</button>
                 </div>
               </div>
+
+
+              </>
             );
           })}
 
-          {/*             <div className="box" data-aos="zoom-in-down">
-                
-                    <img loading="lazy"  src="img/coding-2.jpg" alt="temp" />
-                
-                <h3>Project Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Quaerat voluptate quos odit officia nam eum pariatur, 
-                    consequatur esse neque voluptatem!
-                </p>
-                <button type="submit">Preview</button>
-            </div>
-            <div className="box" data-aos="zoom-in-down" data-aos-delay="300">
-                
-                    <img loading="lazy"  src="img/coding-2.jpg" alt="temp" />
-                
-                <h3>Project Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Quaerat voluptate quos odit officia nam eum pariatur, 
-                    consequatur esse neque voluptatem!
-                </p>
-                <button type="submit">Preview</button>
-            </div>
-            <div className="box" data-aos="zoom-in-down" data-aos-delay="600">
-                
-                    <img loading="lazy"  src="img/coding-2.jpg" alt="temp" />
-                
-                <h3>Project Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Quaerat voluptate quos odit officia nam eum pariatur, 
-                    consequatur esse neque voluptatem!
-                </p>
-                <button type="submit">Preview</button>
-            </div>
-            <div className="box" data-aos="zoom-in-down" data-aos-delay="900">
-                
-                    <img loading="lazy"  src="img/coding-2.jpg" alt="temp" />
-                
-                <h3>Project Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Quaerat voluptate quos odit officia nam eum pariatur, 
-                    consequatur esse neque voluptatem!
-                </p>
-                <button type="submit">Preview</button>
-            </div>
-            <div className="box" data-aos="zoom-in-down" data-aos-delay="1200">
-                
-                    <img loading="lazy"  src="img/coding-2.jpg" alt="temp" />
-                
-                <h3>Project Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Quaerat voluptate quos odit officia nam eum pariatur, 
-                    consequatur esse neque voluptatem!
-                </p>
-                <button type="submit">Preview</button>
-            </div>
-            <div className="box" data-aos="zoom-in-down" data-aos-delay="1500">
-                
-                    <img loading="lazy"  src="img/coding-2.jpg" alt="temp" />
-                
-                <h3>Project Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Quaerat voluptate quos odit officia nam eum pariatur, 
-                    consequatur esse neque voluptatem!
-                </p>
-                <button type="submit">Preview</button>
-            </div> */}
+              <div className='loadMore-div'>
+                { 
+                    itemsID > dataList.length ? (<button className="loadMorebtn" disabled>No more results</button>) : 
+                    (<button className="loadMorebtn" onClick={handleLoadMore}>Load More</button>) 
+                }
+              </div> 
+              
         </div>
       </section>
     </>
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); 
 
 
 export const db = getFirestore(app);
